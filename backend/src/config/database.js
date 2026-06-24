a0f1a15fd9513c9fb31586972278e5bbb59f7d33
RahayuAmaliyahKamisss <xtallmint@gmail.com>
2026-06-24 19:42:09 +0700

    Update 2

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("wisata_db", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+  }
+);
 
 module.exports = sequelize;
